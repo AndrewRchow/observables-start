fcf1f06fe13d4021d7f8a6db3adefb7a9bb88585
Andrew Chow <chowlick@gmail.com>
2018-11-01 11:58:24 -0700

    finish

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, Observable, Observer } from 'rxjs/';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Observable, Observer, Subscription } from 'rxjs/';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy{
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    // const myNumbers = interval(1000);
-    // myNumbers.subscribe(
-    //   (number: number)=> {
-    //     console.log(number);
-    //   }
-    // )
+    const myNumbers = interval(1000);
+    this.numbersObsSubscription = myNumbers.subscribe(
+      (number: number)=> {
+        console.log(number);
+      }
+    );
 
     const myObservable = Observable.create(
       (observer: Observer<string>)=>{
@@ -33,12 +35,16 @@ export class HomeComponent implements OnInit {
           observer.next('second package');
         }, 5000); 
       });
-      myObservable.subscribe(
+      this.customObsSubscription = myObservable.subscribe(
         (data: string) => { console.log(data);},
         (error: string) => { console.log(error);},
         () => {console.log('completed'); }
       );
   }
 
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
+  }
 
 }
